0989ca6a203ab0d3de6bff21f850a5a77b622165
Basty <gajardobastian04@gmail.com>
2025-08-30 10:46:48 -0400

    struct UsuarioTemporal y validacion de campos

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,11 @@
 #ifndef UTILIDADES_H
 #define UTILIDADES_H
 
+#include <string>
+
 void limpiarPantalla();
+void pausarPantalla();
+bool validarLongitudesUsuario(const std::string& nombre, const std::string& username,
+                             const std::string& password);
 
 #endif

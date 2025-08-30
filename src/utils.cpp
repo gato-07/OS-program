570c7115de3f8df614dc74b516c3766ca1a6b553
Basty <gajardobastian04@gmail.com>
2025-08-30 10:45:23 -0400

    validacion de campos para el array de char y funcion pausar pantalla

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,8 @@
 #include "utils.h"
+#include "estructuras.h"
 #include <cstdlib>
+#include <iostream>
+#include <limits>
 
 void limpiarPantalla() {
     #ifdef _WIN32
@@ -7,4 +10,35 @@ void limpiarPantalla() {
     #else
         system("clear");
     #endif
+}
+
+void pausarPantalla() {
+    std::cout << "\nPresione Enter para continuar...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+}
+
+bool validarLongitudesUsuario(const std::string& nombre, const std::string& username,
+                             const std::string& password) {
+
+    const size_t MAX_NOMBRE = sizeof(UsuarioTemporal{}.nombre) - 1;
+    const size_t MAX_USERNAME = sizeof(UsuarioTemporal{}.username) - 1;
+    const size_t MAX_PASSWORD = sizeof(UsuarioTemporal{}.password) - 1;
+
+    if (nombre.length() > MAX_NOMBRE) {
+        std::cerr << "Error: El nombre es demasiado largo (máximo " << MAX_NOMBRE << " caracteres)" << std::endl;
+        return false;
+    }
+
+    if (username.length() > MAX_USERNAME) {
+        std::cerr << "Error: El username es demasiado largo (máximo " << MAX_USERNAME << " caracteres)" << std::endl;
+        return false;
+    }
+
+    if (password.length() > MAX_PASSWORD) {
+        std::cerr << "Error: La contraseña es demasiado larga (máximo " << MAX_PASSWORD << " caracteres)" << std::endl;
+        return false;
+    }
+
+    return true;
 }
